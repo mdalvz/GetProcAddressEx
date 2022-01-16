2becb7631ffad64090f289a9c29d19b3b763a8f8
Malcolm Alvarez <malcolm.alvarez9898@gmail.com>
2022-01-16 11:28:32 -0800

    Removed Explicit Calling Convention

--- include/GetProcAddressEx.h
+++ include/GetProcAddressEx.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <Windows.h>
 
-FARPROC __cdecl GetProcAddressEx(HANDLE _Process, HMODULE _Module, LPCSTR _Name);
+FARPROC GetProcAddressEx(HANDLE _Process, HMODULE _Module, LPCSTR _Name);
 
-FARPROC __cdecl GetProcAddressEx(HANDLE _Process, LPCSTR _Module, LPCSTR _Name);
+FARPROC GetProcAddressEx(HANDLE _Process, LPCSTR _Module, LPCSTR _Name);
--- src/GetProcAddressEx.cpp
+++ src/GetProcAddressEx.cpp
@@ -279,7 +279,7 @@ static HMODULE FindModule(HANDLE _Process, const std::string& _Module) {
 
 }
 
-FARPROC __cdecl GetProcAddressEx(HANDLE _Process, HMODULE _Module, LPCSTR _Name) {
+FARPROC GetProcAddressEx(HANDLE _Process, HMODULE _Module, LPCSTR _Name) {
 	
 	const UINT_PTR base = reinterpret_cast<UINT_PTR>(_Module);
 
@@ -403,7 +403,7 @@ FARPROC __cdecl GetProcAddressEx(HANDLE _Process, HMODULE _Module, LPCSTR _Name)
 
 }
 
-FARPROC __cdecl GetProcAddressEx(HANDLE _Process, LPCSTR _Module, LPCSTR _Name) {
+FARPROC GetProcAddressEx(HANDLE _Process, LPCSTR _Module, LPCSTR _Name) {
 
 	HMODULE module_handle = FindModule(_Process, _Module);
 
